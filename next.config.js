ea63fde7a6e8b42f6b415f03dc48738919209a07
hhhsu <hcxu0814@gmail.com>
2025-05-22 21:09:44 +0800

    nextui

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n: {
+    locales: ["zh-CN", "zh-TW", "en-US"],
+    defaultLocale: "zh-CN",
+  },
+};
+
+module.exports = nextConfig;
